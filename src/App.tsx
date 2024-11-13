92a3cc84d2210d6eef3878b9204151cb1e353985
nasdj21 <nasierra@espol.edu.ec>
2024-11-13 14:28:37 -0500

    Se borro el useCount y los logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import Grid from '@mui/material/Grid2' 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <Grid container spacing={5}>
